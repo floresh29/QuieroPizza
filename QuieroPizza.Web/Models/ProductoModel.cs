ea26775973f8fe94acfc156f7b109df77839826e
ALAN ANDRES FLORES PADILLA <alanpadilla@unah.hn>
2021-09-18 23:19:05 -0600

    Agregando tarea 1

---
+++ QuieroPizza.Web/Models/ProductoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuieroPizza.Web.Models
+{
+    public class ProductoModel
+    {
+        public int id {get; set;}
+        public string descripcion { get; set; }
+        public int Id { get; internal set; }
+    }
+}
